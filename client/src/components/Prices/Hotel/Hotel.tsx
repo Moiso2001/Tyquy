0a20c498cfd2b627a3586c895087a3a0a5ad9cc4
MoisoDavid <wollxi@gmail.com>
2023-09-09 13:04:16 -0500

    Price Hotel

--- client/src/components/Prices/Hotel/Hotel.tsx
+++ client/src/components/Prices/Hotel/Hotel.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Hotel() {
   return (
-    <div>Hotel</div>
+    <div>
+      {/* Title */}
+      <div>
+        <h3>Dog Hotel</h3>
+      </div>
+
+      {/* Description */}
+      <div>
+        <span>Estadia Nocturna</span>
+        <span>$50.000<br/>(cualquier peso o raza)</span>
+      </div>
+    </div>
   )
 }
