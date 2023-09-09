80bd92a85c0eeae9b4a838dcb2e5fbf8a6534ac3
MoisoDavid <wollxi@gmail.com>
2023-09-09 13:06:18 -0500

    Adding Dog Spa on prices

--- client/src/components/Prices/Spa/Spa.tsx
+++ client/src/components/Prices/Spa/Spa.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+// import React from 'react'
 
+/* Icons */
+import {PiWhatsappLogoDuotone} from "react-icons/pi"
+
+/* CSS */
 export default function Spa() {
   return (
-    <div>Spa</div>
+    <div>
+      {/* Title */}
+      <div>
+        <h3>Dog Spa</h3>
+      </div>
+
+      {/* Description */}
+      <div>
+        <div>
+          <p>El precio del spa variara dependiendo de la raza y tamaño del peludito. Escribenos para darte un precio especial!</p>
+        </div>
+        <div>
+          <PiWhatsappLogoDuotone/>
+          <button>Contactanos</button>
+        </div>
+      </div>
+    </div>
   )
 }
