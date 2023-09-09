837f0ef9bce92a83bbc5f70bdc3f8b78b47e818a
MoisoDavid <wollxi@gmail.com>
2023-09-09 14:17:09 -0500

    Including map html

---
+++ client/src/components/Map/Map.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div>
+        {/* Muisca head */}
+        
+        {/* Head */}
+        <div>
+            <h3>Visitanos :)</h3>
+        </div>
+
+        {/* Map Google */}
+        <div>
+        </div>
+    </div>
+  )
+}
