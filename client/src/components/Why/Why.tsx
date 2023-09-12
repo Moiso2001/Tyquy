16c0e6fd41d76a2d0b8654358dce48b75c03da3e
MoisoDavid <wollxi@gmail.com>
2023-09-12 14:37:04 -0500

    Why CSS first texts

--- client/src/components/Why/Why.tsx
+++ client/src/components/Why/Why.tsx
@@ -12,6 +12,7 @@ import styles from "./Why.module.css"
 
 /* Images */
 import  grassBG from "../../assets/grass.png";
+import muiscaMask from "../../assets/tribu decoration/muiscaMask.png"
 
 
 export default function Why() {
@@ -24,14 +25,19 @@ export default function Why() {
           className={styles.div_title}
           style={{backgroundImage: `url(${grassBG})`}}
         >
+          <img 
+            src={muiscaMask}
+            className={styles.title__muiscaMask}
+          />
           <h2> ¿Por qué Tyquy Bayara?</h2>
         </div>
 
         {/* Description and list of services */}
         <div className={styles.div_benefits}>
-          <div>
+          <div className={styles.benefits__div_title}>
             <p>¡Bienvenidos a nuestra guardería canina de confianza! Aquí, su peludo amigo experimentará cuidado de primera clase. Nuestro equipo dedicado ofrece atención personalizada, amplias áreas de juego y un entorno seguro. ¡Elije la felicidad y tranquilidad para tu mascota!</p>
           </div>
+          
           <div>
             {benefits.map(benefit => 
               <Card
