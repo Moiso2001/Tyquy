60cf40fb0d4f3cf399db4a520d63819097c5984b
MoisoDavid <wollxi@gmail.com>
2023-09-12 13:11:21 -0500

    Moving the rune more to left

--- client/src/components/Why/Why.tsx
+++ client/src/components/Why/Why.tsx
@@ -7,19 +7,22 @@ import { benefits, opinions } from "../../utils/constants"
 import Opinion from "../Opinion/Opinion";
 import Card from "./Cards/Card";
 
+/* CSS */
+import styles from "./Why.module.css"
+
 
 export default function Why() {
   const twoOpinions = opinions.filter(opinion => opinion.id > 2);
 
   return (
-    <div>
+    <div className={styles.div_global}>
         {/* Title */}
-        <div>
+        <div className={styles.div_title}>
           <h2> ¿Por qué Tyquy Bayara?</h2>
         </div>
 
         {/* Description and list of services */}
-        <div>
+        <div className={styles.div_benefits}>
           <div>
             <p>¡Bienvenidos a nuestra guardería canina de confianza! Aquí, su peludo amigo experimentará cuidado de primera clase. Nuestro equipo dedicado ofrece atención personalizada, amplias áreas de juego y un entorno seguro. ¡Elije la felicidad y tranquilidad para tu mascota!</p>
           </div>
@@ -34,15 +37,14 @@ export default function Why() {
         </div>
 
         {/* Opinions */}
-        <div>
+        <div className={styles.div_opinion}>
           {twoOpinions.map(opinion => 
             <Opinion
               key={opinion.id}
               {...opinion}
             />)
           }
-        </div>
-        
+        </div>       
     </div>
   )
 }
