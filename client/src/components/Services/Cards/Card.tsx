01864237f291c4560a1b88f52eb1ce33f4ad7209
MoisoDavid <wollxi@gmail.com>
2023-09-13 14:14:11 -0500

    Random decoration

--- client/src/components/Services/Cards/Card.tsx
+++ client/src/components/Services/Cards/Card.tsx
@@ -8,7 +8,10 @@ import rune1 from "../../../assets/tribu decoration/rune1.png"
 import rune2 from "../../../assets/tribu decoration/rune2.png"
 import rune3 from "../../../assets/tribu decoration/rune3.png"
 
-import runeCircle from "../../../assets/tribu decoration/Greek pattern horizontal.png"
+import runeCircle from "../../../assets/tribu decoration/pngwing.com (1) (1).png"
+
+/* Utils and controllers */
+import { getRandomNumber } from '../../../utils/controllers'
 
 
 type CardProps = {
@@ -18,22 +21,52 @@ type CardProps = {
     id: number
 }
 
+type IconsCardProps = {
+    number: number
+}
+
+export function IconsCard({number}: IconsCardProps) {
+    console.log(number)
+    return(
+        <>
+            {number === 1
+             && <>
+                    <img className={styles.rune1} src={rune1}/>
+                    <img className={styles.rune2} src={rune2}/>
+                </>
+            }
+
+            {number === 2
+             && <>
+                    <img className={styles.rune1} src={rune3}/>
+                    <img className={styles.rune2} src={rune1}/>
+                </>
+            }
+
+            {number === 3
+             && <>
+                    <img className={styles.runeCircle1} src={runeCircle}/>
+                </>
+            }
+
+            {number === 4
+             && <>
+                    <img className={styles.runeCircle2} src={runeCircle}/>
+                </>
+            }
+        </>
+    )
+}
+
+
 export default function Card({title,text,img}: CardProps) {
-    const [randomNumber, setRandomNumber] = useState<Number>()
-
-    useEffect(() => {
-        ra
-    }, [])
-
   return (
     <div className={styles.div_global}>
         <img src={img} className={styles.global__image}/>
 
         <div className={styles.div_info}>
             {/* Icons */}
-
-
-
+            <IconsCard number={getRandomNumber(1,4)}/>
 
             <div className={styles.info__div_text}>
                 <span>{title}</span>
