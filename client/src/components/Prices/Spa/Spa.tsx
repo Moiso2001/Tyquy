f528905f3f4b0b4f3b0f09a5e4910927d46efdf6
MoisoDavid <wollxi@gmail.com>
2023-09-13 19:52:23 -0500

    Adding Dog Spa CSS

--- client/src/components/Prices/Spa/Spa.tsx
+++ client/src/components/Prices/Spa/Spa.tsx
@@ -3,22 +3,28 @@
 /* Icons */
 import {PiWhatsappLogoDuotone} from "react-icons/pi"
 
+/* Images and icons */
+import runeLine from "../../../assets/tribu decoration/Greek pattern horizontal.png";
+
 /* CSS */
+import styles from "./Spa.module.css";
+
 export default function Spa() {
   return (
-    <div>
+    <div className={styles.div_global}>
       {/* Title */}
-      <div>
+      <div className={styles.div_title}>
+        <img src={runeLine} className={styles.title__runeIcon}/>
         <h3>Dog Spa</h3>
       </div>
 
       {/* Description */}
-      <div>
+      <div className={styles.div_description}>
         <div>
           <p>El precio del spa variara dependiendo de la raza y tamaño del peludito. Escribenos para darte un precio especial!</p>
         </div>
-        <div>
-          <PiWhatsappLogoDuotone/>
+        <div className={styles.description__div_button}>
+          <PiWhatsappLogoDuotone className={styles.description__icon_whatsapp}/>
           <button>Contactanos</button>
         </div>
       </div>
