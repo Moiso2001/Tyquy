1602442b3150d11871c80d894c7ca35f8d9ccb0a
MoisoDavid <wollxi@gmail.com>
2023-09-13 19:37:25 -0500

    Dog Hotel

--- client/src/components/Prices/Hotel/Hotel.tsx
+++ client/src/components/Prices/Hotel/Hotel.tsx
@@ -1,17 +1,26 @@
 // import React from 'react'
 
+/* CSS */
+import styles from "./Hotel.module.css"
+
+/* Images and icons */
+import runeLine from "../../../assets/tribu decoration/Greek pattern horizontal.png"
+
+
 export default function Hotel() {
   return (
-    <div>
+    <div className={styles.div_global}>
       {/* Title */}
-      <div>
+      <div className={styles.div_title}>
+        <img src={runeLine} className={styles.title__runeIcon}/>
+
         <h3>Dog Hotel</h3>
       </div>
 
       {/* Description */}
-      <div>
+      <div className={styles.div_description}>
         <span>Estadia Nocturna</span>
-        <span>$50.000<br/>(cualquier peso o raza)</span>
+        <span>$50.000<br/><span style={{fontSize: '0.6rem'}}>(cualquier peso o raza)</span></span>
       </div>
     </div>
   )
