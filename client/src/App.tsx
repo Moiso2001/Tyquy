a284550fca74ae6187dc5fd9731714fe94709968
MoisoDavid <wollxi@gmail.com>
2023-10-04 20:28:29 -0500

    Adding routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
 /* React */
 // import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 /* Components */
+import Navbar from './components/Navbar/Navbar'
+import Stepper from './components/Form/Stepper'
 import Home from './components/Home/Home'
 
 /* CSS */
 import styles from "./App.module.css"
-import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className={styles.thediv}>
-      <Navbar/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className={styles.thediv}>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/formulario' element={<Stepper/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/Form/Stepper.tsx
+++ client/src/components/Form/Stepper.tsx
@@ -1,20 +1,8 @@
 /* React */
 import {useState, useEffect} from 'react'
 
-/* Each form step */
-// import Payment from './Steps/Payment';
-// import Second from './Steps/Second';
-// import First from './Steps/First';
-// import Third from './Steps/Third';
-
-/* React Icons */
-import {IoMdHelpCircle} from "react-icons/io";
-// import { InputRequest } from '../../App';
-
 /* CSS */
-import s from "./Stepper.module.css";
-// import HelpWindow from './Steps/HelpWindow';
-
+import styles from "./Stepper.module.css"
 
 export default function Stepper() {
   const [step , setStep] = useState(1);
@@ -47,30 +35,7 @@ export default function Stepper() {
 
 
   return (  
-    <div className={s.divGlobal}>
-
-      {/* This will be the head of our stepper, where the user will see the step on and a charge bar */}
-      <div className={s.divHeader}>
-        <div className={s.divStep}>
-          <span>{`Paso ${step} de 4`}</span>
-          <IoMdHelpCircle className={s.helpIcon} onClick={() => setHelpWindow(true)}/>
-        </div>
-
-        <div className={s.divBar}>
-          <div className={step >= 1 ? s.barActive : s.bar}></div>
-          <div className={step >= 2 ? s.barActive : s.bar}></div>
-          <div className={step >= 3 ? s.barActive : s.bar}></div>
-          <div className={step >= 4 ? s.barActive : s.bar}></div>
-        </div>
-      </div>
-
-      {/* This will be the help window */}
-      {helpWindow &&
-        // <HelpWindow setHelpWindow={setHelpWindow}/>
-      }
-
-      {/* This validation will return the slides depending of the step */}
-      {validation(step)}
+    <div className={styles.divGlobal}>
 
     </div>
   )
