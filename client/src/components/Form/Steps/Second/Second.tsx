18c0f1e73169baba5c513f27e1aa3020f0848d62
MoisoDavid <wollxi@gmail.com>
2023-10-12 19:57:17 -0500

    Adding names form on second

--- client/src/components/Form/Steps/Second/Second.tsx
+++ client/src/components/Form/Steps/Second/Second.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import {useState} from 'react'
+
+/* Icons */
+import {AiOutlinePlusCircle, AiOutlineCloseCircle} from "react-icons/ai";
+
 
 type SecondProps = {
     setStep: React.Dispatch<React.SetStateAction<number>>
 }
 
 export default function Second({setStep}: SecondProps) {
+  const [names,setNames] = useState([{id: 0, name: '', raza: '', edad: 0, peso: 0}])
+
   return (
     <div>
-        
+        <div>
+            {names.map(name => 
+                <div>
+                    <div>
+                        <span>Nombre del perrit@</span>
+                        <input value={name.name}/>
+                    </div>
+
+                    <div>
+                        <span>Raza</span>
+                        <input value={name.raza}/>
+                    </div>
+
+                    <div>
+                        <span>Edad</span>
+                        <input value={name.edad}/>
+                    </div>
+
+                    <div>
+                        <span>Peso</span>
+                        <input value={name.peso}/>
+                    </div>
+
+                    {name.id > 0
+                     ? <AiOutlineCloseCircle/>
+                     : <AiOutlinePlusCircle/>
+                    }
+                </div>
+            )}
+        </div>  
+
+        <div>
+
+        </div>
+
+        <div>
+
+        </div>
     </div>
   )
 }
