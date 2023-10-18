b698a2fb8682665debc38bd0133a646ff31fa342
MoisoDavid <wollxi@gmail.com>
2023-10-17 20:27:32 -0500

    HelpWindow finalized

--- client/src/components/Form/Steps/HelpWindow/HelpWindow.tsx
+++ client/src/components/Form/Steps/HelpWindow/HelpWindow.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default function HelpWindow() {
+/* Icon */
+import {BsArrowRightCircle, BsArrowLeftCircle} from "react-icons/bs"
+
+type HelpWindowProps = {
+    hidden: boolean
+}
+
+export default function HelpWindow({hidden}: HelpWindowProps) {
   return (
-    <div>HelpWindow</div>
+    <div>
+        <div>
+            <p>Queremos conocerte! A continuacion te pediremos cierta 
+                informacion importante para que hagas parte de nuestra 
+                familia, cualquier duda puedes escribirnos a nuestro 
+                whatsapp dando click aqui.
+            </p>
+        </div>
+
+        <div>
+            {hidden
+              ?<BsArrowRightCircle/>
+              :<BsArrowLeftCircle/>
+            }
+        </div>
+    </div>
   )
 }
